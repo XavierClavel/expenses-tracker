f740bf99bb4cf2b87c8c3419b2840d551efccca0
Xavier Clavel <xrclavel@gmail.com>
2025-11-27 16:58:18 +0100

    floating action button to add expenses

--- frontend/app/(tabs)/expenses.tsx
+++ frontend/app/(tabs)/expenses.tsx
@@ -1,11 +1,22 @@
 import { Image } from 'expo-image';
-import {Button, Pressable, Platform, StyleSheet, Text, View} from 'react-native';
+import {Pressable, Platform, StyleSheet, Text, View} from 'react-native';
 
 import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import {Expense} from "@/components/expense";
+import {FAB} from "react-native-paper";
 
 const data = [
+    {value: -900.97, label: 'Accomodation & charges', color: '#009FFF', icon: 'house'},
+    {value: -736.14, label: 'Leisure', color: '#93FCF8', icon: 'train'},
+    {value: -268.40, label: 'Food', color: '#BDB2FA', icon: 'groceries'},
+    {value: -193.38, label: 'Shopping', color: '#FFA5BA', icon: 'trip'},
+    {value: -13.38, label: 'Video games', color: '#e1d481', icon: 'video-games'},
+    {value: -593.78, label: 'School', color: '#b4f1a7', icon: 'school'},
+    {value: -593.78, label: 'Restaurant', color: '#e193d9', icon: 'restaurant'},
+    {value: -593.78, label: 'Restaurant', color: '#efffa5', icon: 'plane'},
+    {value: -593.78, label: 'Restaurant', color: '#bcb8a5', icon: 'car'},
+    {value: -593.78, label: 'Restaurant', color: '#FFA5BA', icon: 'baby'},
     {value: -900.97, label: 'Accomodation & charges', color: '#009FFF', icon: 'house'},
     {value: -736.14, label: 'Leisure', color: '#93FCF8', icon: 'train'},
     {value: -268.40, label: 'Food', color: '#BDB2FA', icon: 'groceries'},
@@ -22,13 +33,12 @@ const data = [
 
 export default function HomeScreen() {
   return (
-
-    <ParallaxScrollView
+      <View style={{ flex: 1}}>
+      <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
           source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
         />
       }>
         <View
@@ -46,24 +56,12 @@ export default function HomeScreen() {
             ))}
         </View>
     </ParallaxScrollView>
+              <FAB
+                  icon="plus"
+                  style={{ position: 'absolute', bottom: 16, alignSelf: 'center', backgroundColor: 'lightgray' }}
+                  onPress={() => {}}
+              />
+
+      </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
